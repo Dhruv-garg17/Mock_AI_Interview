ffbff29242ea4a9f1c8f55f3b5cf8e4495a28456
Dhruv-garg17 <dhruv1689.be21@chitkara.edu.in>
2025-06-08 01:55:29 +0530

    update

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -34,6 +34,7 @@ const generationConfig = {
 export const chatSession = model.startChat({ generationConfig });
 
 app.use("/api", router);
-app.listen(3000, () =>
-  console.log(`server is running at http://localhost:3000`)
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
